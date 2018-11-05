6550c1d61b7da5fd1e004476257ce3d539701fc0
Gustavo C. Santos <gustavosantosixi@gmail.com>
2018-11-05 15:03:20 -0200

    tela cadastro de instituição feito

--- src/br/udesc/ceavi/pin/infosaude/view/frame/FramePrincipal.java
+++ src/br/udesc/ceavi/pin/infosaude/view/frame/FramePrincipal.java
@@ -39,7 +39,7 @@ public class FramePrincipal extends javax.swing.JFrame {
         menu = new MenuJPaneUniversao(this.jpColuna.getSize().width);
         jpMenu.add(menu);
         menu.menuSemUsuario();
-        addPanel(new InternalFrameCampanha());
+        addPanel(new InternalFrameInstituicao());
     }
 
     public void addPanel(JInternalFrame frame) {
